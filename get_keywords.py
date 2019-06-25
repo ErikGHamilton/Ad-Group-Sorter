b3ae043d632d39cca3348b6a132482a144e0e71a
Erik Hamilton <erikghamilton@gmail.com>
2019-06-25 17:05:06 -0400

    Update get_keywords.py

--- get_keywords.py
+++ get_keywords.py
@@ -30,36 +30,59 @@ selector['paging'] = {
 }
 
 
-#TODO : UPDATE THE QUERIES SECTION TO TAKE A USER INPUT - MAKE IT A LIST
+#INPUT THE QUERIES
+input_array = []
+finished_entering = False
+while not finished_entering:
+    user_input = input("Please Enter A Keyword Idea and Type DONE When Finished:   ")
+    if user_input == "done" or user_input == "DONE" or user_input == "Done":
+        finished_entering = True
+    else:
+        input_array.append(user_input)
+
+#breakpoint()
+
 
 selector['searchParameters'] = [{
     'xsi_type': 'RelatedToQuerySearchParameter',
-    'queries': ['space cruise']
+    'queries': input_array
 }]
 
 #breakpoint()
 page = targeting_idea_service.get(selector)
 
 #PARSE THE RESPONSE - THE BELOW IS GOOGLE CODE
+keyword_list = []
 
 for result in page['entries']:
   attributes = {}
   for attribute in result['data']:
     attributes[attribute['key']] = getattr(
         attribute['value'], 'value', '0')
-  print ('Keyword with "%s" text and average monthly search volume '
-         '"%s" was found with Avg CPC: %s.'
-         % (attributes['KEYWORD_TEXT'],
-            attributes['SEARCH_VOLUME'],
-            attributes['AVERAGE_CPC']))
 
-print("ASDF")
-print("ASDF")
-print("ASDF")
+    
 
+  #print ('Keyword with "%s" text and average monthly search volume '
+  #       '"%s" was found with Avg CPC: %s.'
+  #       % (attributes['KEYWORD_TEXT'],
+  #          attributes['SEARCH_VOLUME'],
+  #          attributes['AVERAGE_CPC']))
+  
+  kwrow = {
+      "Keyword": attributes['KEYWORD_TEXT'],
+      "Avg. monthly searches": attributes['SEARCH_VOLUME']
+  }
+  keyword_list.append(kwrow)
 
-parse_entries = page['entries']
+#parse_entries = page['entries']
+#print(parse_entries)
 
-for p,v in parse_entries.items():
-    print("P = ", p)
-    print("V = ", v)
+#for r in keyword_list:
+#    kw = r['Keyword']
+#    searches = r['Avg. monthly searches']
+#    print(kw + " " + str(searches))
+#    breakpoint()
+#   # print("Keyword: "+ kw + " and Searches:" + searches)
+
+print(keyword_list)
+
