294f8e2b0fe292e0c599d12bb2ffd67e56fff32b
ErikGHamilton <50888516+ErikGHamilton@users.noreply.github.com>
2019-07-01 18:43:32 -0400

    Delete sorter_test.py

--- test/sorter_test.py
+++
@@ -1,9 +0,0 @@
-import os
-import pytest
-
-from sorter-v2 import campaign_lookup_function
-
-def test_campaign_lookup:
-    inputkeywordlist = ['parkinsons', 'disease', 'treatment']
-    kwcampaign = campaign_lookup_function(inputkeywordlist)
-    assert kwcampaign == 'Treatment'
